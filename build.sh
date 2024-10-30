fbf92e227a112d520c4e3a170051f8c0af594f47
Murilo Bianco <murilo.bianco@live.com>
2024-10-30 16:01:48 +0000

    build sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 docker builder prune -f
 docker build -t utils_api:latest .
+docker tag utils_api:latest muhrilobianco/api_utils:latest
+docker login
+docker push muhrilobianco/api_utils:latest
